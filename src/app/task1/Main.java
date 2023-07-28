2cbbc5ed91c6cdddebe7c4588cfe099e2a079b35
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-28 23:03:47 +0300

    Add method getData

---
+++ src/app/task1/Main.java
@@ -0,0 +1,16 @@
+package app.task1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static Queue<String> getData(){
+        Queue<String> data = new LinkedList<>();
+        data.add("apricot");
+        data.add("mango");
+        data.add("cherry");
+        data.add("rice");
+        return data;
+    }
+}
