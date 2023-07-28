c0cd1fa6d55b5741e8ec0823d98f80c46ebbd0c6
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-28 23:21:43 +0300

    Add method getData

---
+++ src/app/task2/Main.java
@@ -0,0 +1,15 @@
+package app.task2;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static LinkedList<String> getData(){
+        LinkedList<String> data = new LinkedList<>();
+        data.add("apricot");
+        data.add("mango");
+        data.add("cherry");
+        data.add("rice");
+        return data;
+    }
+}
